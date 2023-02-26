731cddcb088bad20e1a350078915966a6b30c8ba
Alzevir Maicá <alzevirm@gmail.com>
2023-02-26 20:02:44 -0300

    Curso de Vue

---
+++ src/components/Pessoa.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>Esta é a descrição da pessoa:{{ nome }} </h2>
+        <Info/>
+        <Formulario/>
+    </div>
+</template>
+<script>
+   import Info from './Info';
+   import Formulario from './Formulario.vue';
+   export default {
+        name: 'Pessoa',
+        components: {
+          Info,
+          Formulario
+        },
+        data() {
+            return {
+                nome: 'Alzevir'
+            }
+        }
+   }
+</script>
---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+    <h1>Olá Vue!</h1>
+    <p>Meu nome é {{nome}} e trabalho com {{profissao}}</p>
+   </div> 
+</template>
+
+<script>
+   export default {
+    name:'PrimeiroComponente',
+    data() {
+        return {
+            nome:"Alzevir",
+            profissao:"Programador",
+        }
+    }
+  }
+  
+</script>
